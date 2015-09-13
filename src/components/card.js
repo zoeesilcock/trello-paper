25680ba0e65e57c8d4298a8ec43f79fc0e4260a0
Zoee Silcock <mrzoee@gmail.com>
2015-09-13 19:52:43 +0200

    Add a checkbox to cards

--- src/components/card.js
+++ src/components/card.js
@@ -3,16 +3,19 @@ import React from 'react';
 class Card extends React.Component {
   propTypes: {
     id: React.PropTyles.number.isRequired,
-    name: React.PropTypes.string.isRequired
+    name: React.PropTypes.string.isRequired,
+    print: React.PropTypes.bool
   }
 
   render() {
     return (
       <li>
-        <span>{this.props.name}</span>
+        <span><input type="checkbox" defaultChecked={this.props.print} /> {this.props.name}</span>
       </li>
     );
   }
 };
+
+Card.defaultProps = { print: true };
 
 export default Card;
